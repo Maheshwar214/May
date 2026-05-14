9e97304f6e528b5c9732e879ad84d18b62c32d80
Maheshwar <38123268+Maheshwar214@users.noreply.github.com>
2026-05-14 12:14:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function showMessage()
-{
-    alert("Website Successfully Deployed Using Jenkins CI/CD ");
-}
+let cartCount = 0;
+
+const buttons = document.querySelectorAll(".add-cart");
+const cart = document.getElementById("cart-count");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        cartCount++;
+        cart.textContent = cartCount;
+
+        alert("Product added to cart!");
+    });
+});
